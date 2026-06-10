56a3a1b22d2852009a2ffc209e865f0d2325bbfc
auladwd <auladidea@gmail.com>
2026-06-10 22:49:46 +0600

    modified terraform file

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "devops_server" {
   ami           = "ami-0c02fb55956c7d316"
   instance_type = "t2.micro"
-  key_name      = "your-key"
+  key_name      = "my-pairkey.pem"
 
   tags = {
     Name = "DevOps-Monitoring-Server"
